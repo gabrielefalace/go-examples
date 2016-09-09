8b56569030d89c1abdf79b5d16f6779dce4ded92
Gabriele Falace <gabrielefalace@gmail.com>
2016-09-09 10:08:46 +0200

    Update copy.go

--- copy.go
+++ copy.go
@@ -5,51 +5,114 @@ import (
 	"log"
 	"io"
 	"sync"
+	"io/ioutil"
+	"errors"
+	"runtime"
 )
 
 func main() {
 
-	destinationsPaths := []string{"/Users/gfalace/Documents/", "/Users/gfalace/Downloads/"}
-	fileOrigin := "/Users/gfalace/"
-	fileName := "profile.jpg"
+	//TODO read these from input
+	destinationsPaths := []string{"/Users/gfalace/Documents/destination", "/Users/gfalace/Downloads/destination"}
+	fileOrigin := "/Users/gfalace/origin"
+
+	processors := min(len(destinationsPaths), runtime.NumCPU())
+	log.Printf("Runtime using %d processors", processors)
+	runtime.GOMAXPROCS(processors)
 
 	var w sync.WaitGroup
 
 	w.Add(2)
 
 	for _, destination := range destinationsPaths {
-		go func(mountPoint, fileOrigin, fileName string) {
-
-			defer w.Done()
-
-			originalFile, err := os.Open(fileOrigin + fileName)
+		go func(origin, dest string) {
+			log.Printf("in the go routine with params origin: %s  -->  dest: %s", origin, dest)
+			err := copyDir(origin, dest)
 			if err != nil {
-				log.Fatal(err)
+				log.Print(err)
 			}
-			defer originalFile.Close()
-
-			os.Mkdir(mountPoint +"copies", 0777);
-
-			newFile, err := os.Create(mountPoint + "copies/copy_of_" + fileName)
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer newFile.Close()
-
-			// Copy the bytes to destination from source
-			bytesWritten, err := io.Copy(newFile, originalFile)
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Printf("Copied %d bytes.", bytesWritten)
-
-			// Commit (flushes) the file contents to disk
-			err = newFile.Sync()
-			if err != nil {
-				log.Fatal(err)
-			}
-		}(destination, fileOrigin, fileName)
+			log.Printf("Finished copying to destination %s", dest)
+			w.Done()
+		}(fileOrigin, destination)
 	}
 
 	w.Wait()
+}
+
+func copyDir(source, dest string) error {
+
+	fileInfo, err := os.Stat(source)
+	if err != nil {
+		return err
+	}
+
+	if !fileInfo.IsDir() {
+		return errors.New("source is not a folder!")
+	}
+
+	_, err = os.Open(dest)
+	if !os.IsNotExist(err) {
+		return errors.New("destination folder already exists! " + dest)
+	}
+
+	err = os.MkdirAll(dest, fileInfo.Mode())
+	log.Print("created dir: " + dest)
+	if err != nil {
+		return errors.New("could not create detination folder!")
+	}
+
+	elementInDirectory, err := ioutil.ReadDir(source)
+
+	for _, element := range elementInDirectory {
+
+		sourceFilePath := source + "/" + element.Name()
+		destinationPathFile := dest + "/" + element.Name()
+
+		if element.IsDir() {
+			err = copyDir(sourceFilePath, destinationPathFile)
+			if err != nil {
+				log.Println(err)
+			}
+		} else {
+			err = copyFile(sourceFilePath, destinationPathFile)
+			if err != nil {
+				log.Println(err)
+			}
+		}
+
+	}
+	return nil
+}
+
+func copyFile(source, dest string) error {
+	sourceFile, err := os.Open(source)
+	if err != nil {
+		return errors.New("error opening source file: " + source)
+	}
+	defer sourceFile.Close()
+	destFile, err := os.Create(dest)
+	if err != nil {
+		return errors.New("error creating destination file: " + dest)
+	}
+	defer destFile.Close()
+	_, err = io.Copy(destFile, sourceFile)
+	if err == nil {
+		fileInfo, err := os.Stat(source)
+		if err == nil {
+			err = os.Chmod(dest, fileInfo.Mode())
+		} else {
+			errors.New("error getting source stats (while trying to set same permissions as source): " + source)
+		}
+	} else {
+		errors.New("error copying file: " + source)
+	}
+	log.Print("created: " + dest)
+	return nil
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
 }
